cfc87971642d4e1e749f5ae2f4cdd57c8cf80cc8
Rerechan02 <122332019+Rerechan02@users.noreply.github.com>
2023-06-19 20:02:49 +0700

    Add files via upload

---
+++ limit-speed.sh
@@ -0,0 +1,90 @@
+#wget https://github.com/${GitUser}/
+GitUser="Rerechan02"
+#link izin ip vps
+url_izin='https://raw.githubusercontent.com/Rerechan02/iziznscript/main/ip'
+
+# Mendapatkan IP VPS saat ini
+ip_vps=$(curl -s ifconfig.me)
+
+# Mendapatkan isi file izin.txt dari URL
+izin=$(curl -s "$url_izin")
+
+# Memeriksa apakah konten izin.txt berhasil didapatkan
+if [[ -n "$izin" ]]; then
+  while IFS= read -r line; do
+    # Memisahkan nama VPS, IP VPS, dan tanggal kadaluwarsa
+    nama=$(echo "$line" | awk '{print $1}')
+    ipvps=$(echo "$line" | awk '{print $2}')
+    tanggal=$(echo "$line" | awk '{print $3}')
+
+    # Memeriksa apakah IP VPS saat ini cocok dengan IP VPS yang ada di izin.txt
+    if [[ "$ipvps" == "$ip_vps" ]]; then
+      echo "Nama VPS: $nama"
+      echo "IP VPS: $ipvps"
+      echo "Tanggal Kadaluwarsa: $tanggal"
+      break
+    fi
+  done <<< "$izin"
+
+  # Memeriksa apakah IP VPS ditemukan dalam izin.txt
+  if [[ "$ipvps" != "$ip_vps" ]]; then
+    echo "IP VPS tidak ditemukan dalam izin.txt"
+    exit 0
+  fi
+else
+  echo "Konten izin.txt tidak berhasil didapatkan dari URL"
+  exit 0
+fi
+
+clear
+Green_font_prefix="\033[32m" && Red_font_prefix="\033[31m" && Green_background_prefix="\033[42;37m" && Red_background_prefix="\033[41;37m" && Font_color_suffix="\033[0m"
+Info="${Green_font_prefix}[ON]${Font_color_suffix}"
+Error="${Red_font_prefix}[OFF]${Font_color_suffix}"
+cek=$(cat /home/limit)
+NIC=$(ip -o $ANU -4 route show to default | awk '{print $5}');
+function start () {
+echo -e "Limit Speed All Service"
+read -p "Set maximum download rate (in Kbps): " down
+read -p "Set maximum upload rate (in Kbps): " up
+if [[ -z "$down" ]] && [[ -z "$up" ]]; then
+echo > /dev/null 2>&1
+else
+echo "Start Configuration"
+sleep 0.5
+wondershaper -a $NIC -d $down -u $up > /dev/null 2>&1
+systemctl enable --now wondershaper.service
+echo "start" > /home/limit
+echo "Done"
+fi
+}
+function stop () {
+wondershaper -ca $NIC
+systemctl stop wondershaper.service
+echo "Stop Configuration"
+sleep 0.5
+echo > /home/limit
+echo "Done"
+}
+if [[ "$cek" = "start" ]]; then
+sts="${Info}"
+else
+sts="${Error}"
+fi
+clear
+echo -e " \e[0;32m==============================\e[0m"
+echo -e "     \e[1;36mLimit Bandwidth Speed\e[0m"
+echo -e " \e[0;32m==============================\e[0m"
+echo -e " Status $sts"
+echo -e "  1. Start Limit"
+echo -e "  2. Stop Limit"
+echo -e " Press CTRL+C to return"
+read -rp " Please Enter The Correct Number : " -e num
+if [[ "$num" = "1" ]]; then
+start
+elif [[ "$num" = "2" ]]; then
+stop
+else
+clear
+echo " You Entered The Wrong Number"
+menu
+fi
